dd082ab46814e12b4e65fa2de0d6e513f6178f60
spliffrider <raf.buelens@telenet.be>
2025-12-16 08:48:32 +0100

    fix: revert config and pin supabase 2.39.3

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@supabase/supabase-js'], // 👈 Add this line!
 };
 
 export default nextConfig;
