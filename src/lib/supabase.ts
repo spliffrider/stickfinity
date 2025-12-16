70e2adf6aee9de1d7ec108a32201f90ccd302351
spliffrider <raf.buelens@telenet.be>
2025-12-16 08:42:03 +0100

    fix: standard supabase import

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+import { Database } from './database.types';
 
-import { createClient } from '@supabase/supabase-js'
-import { Database } from './database.types'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
