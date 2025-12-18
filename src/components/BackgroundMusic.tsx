45d13103f85be3aab6e24a7338d8bca153dc599a
spliffrider <raf.buelens@telenet.be>
2025-12-18 08:58:48 +0100

    fix(ui): move music player up to clear bottom toolbar

--- src/components/BackgroundMusic.tsx
+++ src/components/BackgroundMusic.tsx
@@ -37,7 +37,7 @@ export default function BackgroundMusic() {
     };
 
     return (
-        <div className="fixed bottom-12 left-1/2 -translate-x-1/2 z-50 flex flex-col items-center gap-2">
+        <div className="fixed bottom-32 left-1/2 -translate-x-1/2 z-50 flex flex-col items-center gap-2">
             {/* Credit Link */}
             <a
                 href="https://john-b.bandcamp.com/track/up-all-night-epic-mix-2020-remaster"
